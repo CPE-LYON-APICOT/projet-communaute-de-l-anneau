cc5aeaf5c9266c3ecf3f597608a725bea928a530
Tristan <t.roux@cpe.fr>
2026-03-31 16:58:36 +0200

    amélioration du GameManager

--- src/main/java/fr/cpe/engine/GameManager.java
+++ src/main/java/fr/cpe/engine/GameManager.java
@@ -11,25 +11,62 @@ import java.util.List;
 @Singleton
 public class GameManager {
 
-    private Joueur joueur1;
-    private Joueur joueur2;
-    private Pyramide pyramide;
-    private List<GameObserver> observers = new ArrayList<>();
-    private List<Specification<GameManager>> victorySpecs = new ArrayList<>();
+    private final Joueur joueur1;
+    private final Joueur joueur2;
+    private final Pyramide pyramide;
+    
+    private final List<GameObserver> observers = new ArrayList<>();
+    private final List<Specification<GameManager>> victorySpecs = new ArrayList<>();
 
     @Inject
     public GameManager() {
-        this.joueur1 = null;
-        this.joueur2 = null;
-        this.pyramide = null;
+        this.joueur1 = new Joueur();
+        this.joueur2 = new Joueur();
+        this.pyramide = new Pyramide();
+        
+        this.victorySpecs.add(new AllianceSpecification());
+        this.victorySpecs.add(new RingSpecification());
     }
 
-    public void acheterCarte(Carte c) {
+    public Joueur getJoueur1() {
+        return joueur1;
+    }
+
+    public Joueur getJoueur2() {
+        return joueur2;
+    }
+
+    public Pyramide getPyramide() {
+        return pyramide;
+    }
+
+    public void addObserver(GameObserver observer) {
+        this.observers.add(observer);
     }
 
     public void notifierObservers() {
+        for (GameObserver observer : observers) {
+            observer.onGameStateChanged();
+        }
     }
 
-    public void verifierVictoire() {
+    public void acheterCarte(Carte c, Joueur acheteur) {
+        if (pyramide.estLibre(c)) {
+            pyramide.retirerCarte(c);
+            
+            // TODO: Ajouter la logique d'application des effets de la carte au joueur
+            
+            verifierVictoire();
+            notifierObservers();
+        }
+    }
+
+    public boolean verifierVictoire() {
+        for (Specification<GameManager> spec : victorySpecs) {
+            if (spec.isSatisfiedBy(this)) {
+                return true; // Une condition de victoire a été atteinte
+            }
+        }
+        return false;
     }
 }
