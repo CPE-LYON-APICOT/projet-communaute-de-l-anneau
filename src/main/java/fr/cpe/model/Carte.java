86ba9d955f997823466a16f30f1efbce03cc545e
tristanroux <162288438+tristanrouxdev@users.noreply.github.com>
2026-04-23 20:01:13 +0200

    feat(carte): ajouter champ avanceAnneau pour la piste de l'Anneau

--- src/main/java/fr/cpe/model/Carte.java
+++ src/main/java/fr/cpe/model/Carte.java
@@ -8,18 +8,26 @@ public class Carte {
     private final String symboleAlliance;
     private final String symboleCompetence;
     private final String symboleRequis;
+    private final int avanceAnneau;
 
-    public Carte(String nom, String couleur, int coutOr, String symboleAlliance, String symboleCompetence, String symboleRequis) {
+    public Carte(String nom, String couleur, int coutOr, String symboleAlliance,
+                 String symboleCompetence, String symboleRequis, int avanceAnneau) {
         this.nom = nom;
         this.couleur = couleur;
         this.coutOr = coutOr;
         this.symboleAlliance = symboleAlliance;
         this.symboleCompetence = symboleCompetence;
         this.symboleRequis = symboleRequis;
+        this.avanceAnneau = avanceAnneau;
+    }
+
+    public Carte(String nom, String couleur, int coutOr, String symboleAlliance,
+                 String symboleCompetence, String symboleRequis) {
+        this(nom, couleur, coutOr, symboleAlliance, symboleCompetence, symboleRequis, 0);
     }
 
     public Carte(String nom, String couleur, int coutOr, String symboleAlliance) {
-        this(nom, couleur, coutOr, symboleAlliance, null, null);
+        this(nom, couleur, coutOr, symboleAlliance, null, null, 0);
     }
 
     public String getNom() {
@@ -44,5 +52,9 @@ public class Carte {
 
     public String getSymboleRequis() {
         return symboleRequis;
+    }
+
+    public int getAvanceAnneau() {
+        return avanceAnneau;
     }
 }
